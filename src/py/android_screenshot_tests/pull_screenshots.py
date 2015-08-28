54da326943170cc39cb3f8de95c424f753f09171
Arnold Noronha <arnold@fb.com>
2015-08-28 15:56:38 -0400

    okay now we dont need to specify target

--- src/py/android_screenshot_tests/pull_screenshots.py
+++ src/py/android_screenshot_tests/pull_screenshots.py
@@ -21,7 +21,9 @@ import getopt
 import shutil
 from . import metadata
 from .simple_puller import SimplePuller
+import glob
 
+from sets import Set
 from os.path import join
 from os.path import abspath
 
@@ -172,12 +174,48 @@ def pull_screenshots(process,
         print('Open the following url in a browser to view the results: ')
         print('  file://%s' % path_to_html)
 
+def _check_output(args, **kwargs):
+    with open(os.devnull) as f:
+        kwargs['stderr'] = f
+        return subprocess.check_output(args, **kwargs)
+
+def parse_package_line(line):
+    """The line looks like this:
+    package: name='com.facebook.testing.tests' versionCode='1' versionName=''"""
+
+    for word in line.split():
+        if word.startswith("name='"):
+            return word[len("name='"):-1]
+
+def get_aapt_bin():
+    """Find the binary for aapt from $ANDROID_SDK"""
+    android_sdk = os.environ.get('ANDROID_SDK') or os.environ.get('ANDROID_HOME')
+    build_tools = os.path.join(android_sdk, 'build-tools')
+
+    all = list(glob.glob(os.path.join(build_tools, '*/aapt')))
+    bad = list(glob.glob(os.path.join(build_tools, 'android-*/aapt')))
+    good = list(Set(all) - Set(bad))
+
+    good.sort()
+    bad.sort()
+
+    if len(good) == 0:
+        return bad[-1]
+
+    return good[-1]
+
+def get_package(apk):
+    output = _check_output([get_aapt_bin(), 'dump', 'badging', apk], stderr=os.devnull)
+    for line in output.split('\n'):
+        if line.startswith('package:'):
+            return parse_package_line(line)
+
 def main(argv):
     try:
         opt_list, rest_args = getopt.gnu_getopt(
             argv[1:],
             "eds:",
-            ["generate-png=", "filter-name-regex="])
+            ["generate-png=", "filter-name-regex=", "apk"])
     except getopt.GetoptError, err:
         usage()
         return 2
@@ -187,7 +225,13 @@ def main(argv):
         return 2
 
     process = rest_args[0]  # something like com.facebook.places.tests
+
     opts = dict(opt_list)
+
+    if "--apk" in opts:
+        # treat process as an apk instead
+        process = get_package(process)
+
     puller_args = []
     if "-e" in opts:
         puller_args.append("-e")
