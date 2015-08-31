e369693f79e38d22103b0fc8c745e6e4d2948908
Arnold Noronha <arnold@fb.com>
2015-08-31 13:23:12 -0400

    rename

--- src/main/java/com/facebook/testing/screenshot/internal/TestNameDetector.java
+++ src/main/java/com/facebook/testing/screenshot/internal/TestNameDetector.java
@@ -7,7 +7,7 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-package com.facebook.testing.screenshot;
+package com.facebook.testing.screenshot.internal;
 
 import java.lang.reflect.Method;
 
--- src/main/java/com/facebook/testing/screenshot/internal/Tiling.java
+++ src/main/java/com/facebook/testing/screenshot/internal/Tiling.java
@@ -7,7 +7,7 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-package com.facebook.testing.screenshot;
+package com.facebook.testing.screenshot.internal;
 
 /**
  * A 2D layout of image tiles. We represent images as strings which
