a1b5b938fea3f11bce69ab1785e18c63d19ab9f5
Arnold Noronha <arnold@fb.com>
2015-09-22 10:56:23 -0400

    Make first test

---
+++ plugin/src/test/groovy/com/facebook/testing/screenshot/build/ScreenshotsPluginTest.groovy
@@ -0,0 +1,11 @@
+package com.facebook.testing.screenshot.build;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+class ScreenshotsPluginTest {
+  @Test
+  public void testOne() {
+    assertEquals(true, true);
+  }
+}
