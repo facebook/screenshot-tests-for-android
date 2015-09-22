f61612a31171745ecac096acae2b3f96f321abe0
Arnold Noronha <arnold@fb.com>
2015-09-22 13:38:15 -0400

    forgot the test runner in last commit

---
+++ core/src/androidTest/java/com/facebook/testing/screenshot/CustomScreenshotTestRunner.java
@@ -0,0 +1,21 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+package com.facebook.testing.screenshot;
+
+import android.os.Bundle;
+import android.test.InstrumentationTestRunner;
+import android.support.test.runner.AndroidJUnitRunner;
+
+public class CustomScreenshotTestRunner extends AndroidJUnitRunner {
+  @Override
+  public void onCreate(Bundle args) {
+    ScreenshotRunner.onCreate(this, args);
+    super.onCreate(args);
+  }
+
+  @Override
+  public void finish(int resultCode, Bundle results) {
+    ScreenshotRunner.onDestroy();
+    super.finish(resultCode, results);
+  }
+}
