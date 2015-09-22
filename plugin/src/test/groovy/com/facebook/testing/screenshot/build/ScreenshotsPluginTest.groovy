a69e9522b5536a8872cb3e4d3e3f596bc235f712
Arnold Noronha <arnold@fb.com>
2015-09-22 10:59:12 -0400

    test

--- plugin/src/test/groovy/com/facebook/testing/screenshot/build/ScreenshotsPluginTest.groovy
+++ plugin/src/test/groovy/com/facebook/testing/screenshot/build/ScreenshotsPluginTest.groovy
@@ -4,13 +4,19 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 class ScreenshotsPluginTest {
+  def plugin;
+
+  @Before
+  public void setup() {
+    plugin = new ScreenshotsPlugin()
+  }
+
   @Test
   public void testOne() {
-    assertEquals(true, true);
+    assertEquals(true, true)
   }
 
   @Test
   public void testConstructor() {
-    new ScreenshotsPlugin();
   }
 }
