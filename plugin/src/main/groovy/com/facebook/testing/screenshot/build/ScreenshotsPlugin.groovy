422437a7e406bebbc837455ebeea7f6cf4724e24
Arnold Noronha <arnold@fb.com>
2016-05-16 18:24:10 -0400

    Version bump

--- plugin/src/main/groovy/com/facebook/testing/screenshot/build/ScreenshotsPlugin.groovy
+++ plugin/src/main/groovy/com/facebook/testing/screenshot/build/ScreenshotsPlugin.groovy
@@ -140,7 +140,7 @@ class ScreenshotsPlugin implements Plugin<Project> {
 
     if (!implementationVersion) {
       println("WARNING: you shouldn't see this in normal operation, file a bug report if this is not a framework test")
-      implementationVersion = '0.4'
+      implementationVersion = '0.4.1'
     }
 
     project.dependencies.androidTestCompile('com.facebook.testing.screenshot:core:' + implementationVersion)
