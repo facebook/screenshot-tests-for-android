60b7b8bd246973aa57d5cac96fd4af7438a9cdea
Arnold Noronha <arnold@fb.com>
2016-07-12 16:47:51 -0400

    test

--- plugin/src/test/java/com/facebook/testing/screenshot/WebServerTest.java
+++ plugin/src/test/java/com/facebook/testing/screenshot/WebServerTest.java
@@ -2,23 +2,34 @@
 
 package com.facebook.testing.screenshot;
 
-import org.junit.Before;
-import org.junit.After;
-import org.junit.Test;
-import static org.junit.Assert.*;
-import java.net.URL;
 import java.io.*;
+import java.net.URL;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 public class WebServerTest {
   int PORT = 10021;
 
   private WebServer mWebServer;
 
+  @Rule
+  public TemporaryFolder mTempDir = new TemporaryFolder();
+
   @Before
   public void before() throws Throwable {
     mWebServer = new WebServer(PORT);
     mWebServer.start();
+
+    File file = mTempDir.newFile("metadata.xml");
+    Files.write("<screenshots></screenshots>", file, Charsets.UTF_8);
   }
 
   @After
