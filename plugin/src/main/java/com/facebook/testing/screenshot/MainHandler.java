1d9a520851c956db26e9fb7b926fd3dd3122a042
Arnold Noronha <arnold@fb.com>
2016-07-14 16:46:23 -0400

    test

--- plugin/src/main/java/com/facebook/testing/screenshot/MainHandler.java
+++ plugin/src/main/java/com/facebook/testing/screenshot/MainHandler.java
@@ -14,6 +14,15 @@ import org.xml.sax.*;
 import org.xml.sax.helpers.DefaultHandler;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
+
 public class MainHandler extends AbstractHandler {
   private File mRoot;
 
@@ -42,9 +51,9 @@ public class MainHandler extends AbstractHandler {
     writer.write("<head>");
     writer.write("<script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>");
     writer.write("<script src='https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js'></script>");
-    writer.write("<script src='default.js'></script>");
+    writer.write("<script src='/public/default.js'></script>");
     writer.write("<link rel='stylesheet' href='https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/themes/smoothness/jquery-ui.css' />");
-    writer.write("<link rel='stylesheet' href='default.css'></head>");
+    writer.write("<link rel='stylesheet' href='/public/default.css'></head>");
 
     writer.println("<html>");
 
@@ -58,19 +67,100 @@ public class MainHandler extends AbstractHandler {
     baseRequest.setHandled(true);
   }
 
-  private void printDetails(File metadata, PrintWriter writer) {
-    SAXParserFactory factory = SAXParserFactory.newInstance();
+  static class Element {
+    String name;
+  }
 
+  private void printDetails(File metadata, final PrintWriter writer) {
+    if (!metadata.exists()) {
+      writer.println("could not find metadata file");
+      return;
+    }
+    boolean alternate = false;
     try {
-      SAXParser parser = factory.newSAXParser();
-      parser.parse(new FileInputStream(metadata), new DefaultHandler() {
-          @Override
-          public void startElement(String uri, String localName, String qName, Attributes attr) {
-            writer.println("foo");
-          }
-        });
-    } catch (ParserConfigurationException|IOException|SAXException e) {
+      DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder builder = dbFactory.newDocumentBuilder();
+      Document doc = builder.parse(metadata);
+
+      NodeList list = doc.getElementsByTagName("screenshot");
+
+      for (int i = 0; i < list.getLength(); i++) {
+        Node node = list.item(i);
+        alternate = !alternate;
+        writer.println(String.format("<div class='screenshot %s'>", alternate ? "alternate" : ""));
+        writer.println("<div class='screenshot_name'>");
+
+        writeExtra(node ,writer);
+        writer.println(getChildNode(node, "name").getTextContent());
+        writer.println("</div>");
+
+        Node description = getChildNode(node, "description");
+        if (description != null) {
+          writer.println("<div class='screenshot_description'>");
+          writer.println(description.getTextContent());
+          writer.println("</div>");
+        }
+
+
+        if (getChildNode(node, "error") != null) {
+          writer.println("<div class='screenshot_error'>" + getChildNode(node, "error").getTextContent() + "</div>");
+        } else {
+          writeImage(node, writer);
+        }
+
+        writer.println("</div>");
+      }
+
+      writer.println("END");
+    } catch (SAXException|ParserConfigurationException|IOException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  private void writeExtra(Node node, PrintWriter writer) {
+    Node extras = getChildNode(node, "extras");
+    if (extras == null) {
+      return;
+    }
+
+    String str = "";
+    for (int i = 0; i < extras.getChildNodes().writeImagewriteImagegetLength(); i++) {
+      writer.println("extras unsupported");
+    }
+  }
+
+  private void writeImage(Node node, PrintWriter writer) {
+    writer.println("<table class='img-wrapper'>");
+    for (int y = 0; y < Integer.parseInt(getChildNode(node, "tile_height").getTextContent());
+         y++) {
+      writer.println("<tr>");
+      for (int x = 0; x < Integer.parseInt(getChildNode(node, "tile_width").getTextContent());
+           x++) {
+        writer.println("<td>");
+        String fileName = getTileName(getChildNode(node, "name").getTextContent(), x, y);
+        writer.println("<img src='/s/" + fileName + ".png' />");
+        writer.println("</td>");
+      }
+      writer.println("</tr>");
+    }
+    writer.println("</table>");
+  }
+
+  private String getTileName(String name, int x, int y) {
+    if (x == 0 && y == 0) {
+      return name;
+    }
+    return name + "_" + x + "_" + y;
+  }
+
+  private Node getChildNode(Node node, String name) {
+    NodeList list = node.getChildNodes();
+    for (int i = 0; i < list.getLength(); i++) {
+      if (list.item(i).getNodeName().equals(name)) {
+        return list.item(i);
+      }
+    }
+
+    return null;
   }
 }
