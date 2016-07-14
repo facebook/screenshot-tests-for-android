93d7c7ae91683e768b54126cc18aa46c98b0cea1
Arnold Noronha <arnold@fb.com>
2016-07-14 14:35:36 -0400

    test

--- plugin/src/main/java/com/facebook/testing/screenshot/WebServer.java
+++ plugin/src/main/java/com/facebook/testing/screenshot/WebServer.java
@@ -7,30 +7,39 @@ import java.util.*;
 
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 
 public class WebServer {
   private int mPort;
   private Server mServer;
-  private File mMetatdataFile;
+  private File mRoot;
 
   public WebServer(int port) {
     mPort = port;
   }
 
   public void setRoot(File file) {
-    mMetatdataFile = file;
+    mRoot = file;
   }
 
   public void start() {
     mServer = new Server(mPort);
 
-    Handler rootHandler = new MainHandler(mMetatdataFile);
+    Handler rootHandler = new MainHandler(mRoot);
     ContextHandler rootContextHandler = new ContextHandler("/");
     rootContextHandler.setHandler(rootHandler);
 
+    ResourceHandler resourceHandler = new ResourceHandler();
+
+    if (mRoot != null) {
+      resourceHandler.setDirectoriesListed(true);
+      resourceHandler.setResourceBase(mRoot.getAbsolutePath());
+    }
+
     Handler[] handlers = new Handler[] {
-      rootContextHandler
+      rootContextHandler,
+      resourceHandler,
     };
 
     ContextHandlerCollection contexts = new ContextHandlerCollection();
