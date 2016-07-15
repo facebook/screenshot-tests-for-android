e3cabf8bac748e5bb91df6061f99d340820c69e4
Arnold Noronha <arnold@fb.com>
2016-07-15 15:19:09 -0400

    test

---
+++ plugin/src/main/java/com/facebook/testing/screenshot/RemoteFileHandler.java
@@ -0,0 +1,41 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+package com.facebook.testing.screenshot;
+
+import org.eclipse.jetty.server.handler.*;
+import com.android.ddmlib.IDevice;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.parsers.*;
+import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+import org.eclipse.jetty.server.*;
+import org.xml.sax.*;
+import org.xml.sax.helpers.DefaultHandler;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
+public class RemoteFileHandler extends AbstractHandler {
+  public RemoteFileHandler(IDevice device) {
+  }
+
+  @Override
+  public void handle(
+      String target,
+      Request baseRequest,
+      HttpServletRequest request,
+      HttpServletResponse response) throws IOException, ServletException {
+
+  }
+}
---
+++ plugin/src/test/java/com/facebook/testing/screenshot/RemoteFileHandlerTest.java
@@ -0,0 +1,30 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+package com.facebook.testing.screenshot;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.Rule;
+import static org.junit.Assert.fail;
+import com.android.ddmlib.IDevice;
+
+public class RemoteFileHandlerTest {
+  private IDevice device;
+
+  @Before
+  public void before() throws Throwable {
+    device = mock(IDevice.class);
+  }
+
+  @Test
+  public void testHandleRequest() throws Throwable {
+
+  }
+}
