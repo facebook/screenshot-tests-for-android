55d5790d11c8e61fd52a1fad17283787ca2b1ce8
Guardiola31337 <pablo.guardiola@mapbox.com>
2017-06-16 16:50:14 +0200

    add working texture map example

--- examples/app-example-androidjunitrunner/app/src/main/java/com/example/screenshots/MapboxSimpleMapActivity.java
+++ examples/app-example-androidjunitrunner/app/src/main/java/com/example/screenshots/MapboxSimpleMapActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.MarkerView;
+import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
@@ -12,6 +15,14 @@ public class MapboxSimpleMapActivity extends AppCompatActivity {
 
   private MapboxMap map;
   private MapView mapView;
+  private static final LatLng[] LAT_LNGS = new LatLng[] {
+    new LatLng(38.897424, -77.036508),
+    new LatLng(38.909698, -77.029642),
+    new LatLng(38.907227, -77.036530),
+    new LatLng(38.905607, -77.031916),
+    new LatLng(38.889441, -77.050134),
+    new LatLng(38.888000, -77.050000) // Slight overlap to show re-ordering on selection
+  };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +39,17 @@ public class MapboxSimpleMapActivity extends AppCompatActivity {
       @Override
       public void onMapReady(MapboxMap mapboxMap) {
         map = mapboxMap;
+        mapboxMap.setAllowConcurrentMultipleOpenInfoWindows(true);
+        // add default ViewMarker markers
+        for (int i = 0; i < LAT_LNGS.length; i++) {
+          MarkerView marker = mapboxMap.addMarker(new MarkerViewOptions()
+            .position(LAT_LNGS[i])
+            .title(String.valueOf(i))
+          );
+          if (i % 2 == 0) {
+            mapboxMap.selectMarker(marker);
+          }
+        }
       }
     });
   }
