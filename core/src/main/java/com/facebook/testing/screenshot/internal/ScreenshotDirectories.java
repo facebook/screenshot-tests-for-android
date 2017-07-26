64b69db62f4fb34fe73c90ca5b14a8933d652717
Sridhar Vadlamani <v.sridhar.sreenivas@gmail.com>
2017-07-25 17:17:15 -0700

    Revert scope change

--- core/src/main/java/com/facebook/testing/screenshot/internal/ScreenshotDirectories.java
+++ core/src/main/java/com/facebook/testing/screenshot/internal/ScreenshotDirectories.java
@@ -22,7 +22,7 @@ import java.io.File;
 class ScreenshotDirectories {
   private Context mContext;
 
-  ScreenshotDirectories(Context context) {
+  public ScreenshotDirectories(Context context) {
     mContext = context;
   }
 
