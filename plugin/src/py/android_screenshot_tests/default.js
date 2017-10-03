8b7089f5d776f309a686b0e02f53ecafc98600a1
Hilal Alsibai <xiphirx@users.noreply.github.com>
2017-10-03 16:19:41 -0400

    Spruce up Results Page
    The screenshot test result page looked a little sad / bland before, this
    makes it look better (tm). I also inlined the view hierarchy since the
    previous button to view it didnt work, and it didnt make much sense not
    to. I also plan on converting the XML into a collapsable hierarchy at
    some point, but this is a quick start on a redesign of the page

--- plugin/src/py/android_screenshot_tests/default.js
+++ plugin/src/py/android_screenshot_tests/default.js
@@ -1,20 +1,4 @@
 $(function () {
-    $(".view_dump").click(function () {
-        var name = $(this).attr('data-name');
-        $.ajax(
-            {
-                url: name + "_dump.xml",
-                dataType: "text",
-                success: function(result) {
-                    window.alert(result);
-                },
-                error: function(result) {
-                    window.alert("could not load the view hierarchy for " + name);
-                }
-            });
-
-    });
-
     $(".extra").click(function () {
             var str = $(this).attr('data');
             $('<pre></pre>').dialog({
