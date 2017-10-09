54f15b55d54d2ef9d2936bf08144e9164909ccdb
dimorinny <didika914@gmail.com>
2017-10-09 21:02:34 +0300

    specify test package name explicitly instead of parsing it from test apk

--- plugin/src/main/groovy/com/facebook/testing/screenshot/build/ScreenshotsPlugin.groovy
+++ plugin/src/main/groovy/com/facebook/testing/screenshot/build/ScreenshotsPlugin.groovy
@@ -9,6 +9,8 @@ class ScreenshotsPluginExtension {
     def recordDir = "screenshots"
     def addCompileDeps = true
 
+    def testPackage = ""
+
     // Only used for the pullScreenshotsFromDirectory task
     def referenceDir = ""
     def targetPackage = ""
@@ -30,8 +32,8 @@ class ScreenshotsPlugin implements Plugin<Project> {
     def recordMode = false
     def verifyMode = false
 
-    def codeSource = ScreenshotsPlugin.class.getProtectionDomain().getCodeSource();
-    def jarFile = new File(codeSource.getLocation().toURI().getPath());
+    def codeSource = ScreenshotsPlugin.class.getProtectionDomain().getCodeSource()
+    def jarFile = new File(codeSource.getLocation().toURI().getPath())
 
     // We'll figure out the adb in afterEvaluate
     def adb = null
@@ -45,12 +47,10 @@ class ScreenshotsPlugin implements Plugin<Project> {
       description = "Pull screenshots from the device"
       doLast {
         project.exec {
-          def output = getTestApkOutput(project)
-
           executable = 'python'
           environment('PYTHONPATH', jarFile)
 
-          args = ['-m', 'android_screenshot_tests.pull_screenshots', "--apk", output.toString()]
+          args = ['-m', 'android_screenshot_tests.pull_screenshots', "--package", output.toString()]
 
           def referenceDir = project.screenshots.referenceDir
           if(referenceDir) {
@@ -144,13 +144,6 @@ class ScreenshotsPlugin implements Plugin<Project> {
         verifyMode = true
       }
     }
-  }
-
-  String getTestApkOutput(Project project) {
-
-    return project.tasks.getByPath(project.screenshots.testApkTarget).getOutputs().getFiles().filter {
-      it.getAbsolutePath().endsWith ".apk"
-    }.getSingleFile().getAbsolutePath()
   }
 
   void printPullFromDirectoryUsage(def logger, def referenceDir, def targetPackage) {
--- plugin/src/py/android_screenshot_tests/pull_screenshots.py
+++ plugin/src/py/android_screenshot_tests/pull_screenshots.py
@@ -24,7 +24,6 @@ import json
 from . import metadata
 from .simple_puller import SimplePuller
 import zipfile
-from . import aapt
 from . import common
 
 from os.path import join
@@ -322,7 +321,7 @@ def _validate_metadata(dir):
     except ET.ParseError as e:
         raise RuntimeError("Unable to parse metadata file, this commonly happens if you did not call ScreenshotRunner.onDestroy() from your instrumentation")
 
-def pull_screenshots(process,
+def pull_screenshots(package,
                      adb_puller,
                      perform_pull=True,
                      temp_dir=None,
@@ -342,7 +341,10 @@ def pull_screenshots(process,
     copy_assets(temp_dir)
 
     if perform_pull is True:
-        pull_filtered(process, adb_puller=adb_puller, dir=temp_dir, filter_name_regex=filter_name_regex)
+        pull_filtered(package=package,
+                      adb_puller=adb_puller,
+                      dir=temp_dir,
+                      filter_name_regex=filter_name_regex)
 
     _validate_metadata(temp_dir)
 
@@ -377,7 +379,7 @@ def main(argv):
         opt_list, rest_args = getopt.gnu_getopt(
             argv[1:],
             "eds:",
-            ["generate-png=", "filter-name-regex=", "apk", "record=", "verify=", "temp-dir=", "no-pull"])
+            ["generate-png=", "filter-name-regex=", "package", "record=", "verify=", "temp-dir=", "no-pull"])
     except getopt.GetoptError as err:
         usage()
         return 2
@@ -386,13 +388,12 @@ def main(argv):
         usage()
         return 2
 
-    process = rest_args[0]  # something like com.facebook.places.tests
+    package = rest_args[0]  # something like com.facebook.places.tests
+
+    if not package:
+        raise RuntimeError("testPackage property must be specified in your gradle settings")
 
     opts = dict(opt_list)
-
-    if "--apk" in opts:
-        # treat process as an apk instead
-        process = aapt.get_package(process)
 
     should_perform_pull = ("--no-pull" not in opts)
 
@@ -406,7 +407,7 @@ def main(argv):
     if "-s" in opts:
         puller_args += ["-s", opts["-s"]]
 
-    return pull_screenshots(process,
+    return pull_screenshots(package=package,
                             perform_pull=should_perform_pull,
                             temp_dir=opts.get('--temp-dir'),
                             filter_name_regex=opts.get('--filter-name-regex'),
