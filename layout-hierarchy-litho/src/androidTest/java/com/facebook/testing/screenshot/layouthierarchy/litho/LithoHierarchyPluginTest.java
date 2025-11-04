d10b6e5ddc54f8e2bb4c2cbe01a82035c67f73fc
generatedunixname537391475639613 <noreply+537391475639613@fb.com>
2025-11-03 17:55:27 -0800

    fbandroid/libraries/screenshot-tests-for-android/layout-hierarchy-litho/src/androidTest/java/com/facebook/testing/screenshot/layouthierarchy/litho/LithoHierarchyPluginTest.java
    Reviewed By: ianlevesque
    
    Differential Revision: D86085672
    
    fbshipit-source-id: cf4956eb6057c65f60368ad2817efe9721de3dee

--- layout-hierarchy-litho/src/androidTest/java/com/facebook/testing/screenshot/layouthierarchy/litho/LithoHierarchyPluginTest.java
+++ layout-hierarchy-litho/src/androidTest/java/com/facebook/testing/screenshot/layouthierarchy/litho/LithoHierarchyPluginTest.java
@@ -16,7 +16,7 @@
 
 package com.facebook.testing.screenshot.layouthierarchy.litho;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import android.widget.ImageView;
 import androidx.test.InstrumentationRegistry;
@@ -31,12 +31,12 @@ public class LithoHierarchyPluginTest {
   @Test
   public void testAcceptsLithoView() throws Exception {
     LithoView lithoView = new LithoView(InstrumentationRegistry.getTargetContext());
-    assertEquals(true, LithoHierarchyPlugin.getInstance().accept(lithoView));
+    assertThat(LithoHierarchyPlugin.getInstance().accept(lithoView)).isTrue();
   }
 
   @Test
   public void testDoesntAcceptOtherViews() {
     ImageView imageView = new ImageView(InstrumentationRegistry.getTargetContext());
-    assertEquals(false, LithoHierarchyPlugin.getInstance().accept(imageView));
+    assertThat(LithoHierarchyPlugin.getInstance().accept(imageView)).isFalse();
   }
 }
